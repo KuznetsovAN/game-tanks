cfd5b3e1ec5a10b44225beadb42fabb7cfd9695a
alexander.kuznetsov <alexander.kuznetsov@ustrong.ru>
2018-04-05 14:20:22 +0300

    socket начало

--- src/game-visual/app/app.js
+++ src/game-visual/app/app.js
@@ -1,9 +1,28 @@
 import './app.less';
-import Vue  from 'vue';
+//import Vue  from 'vue';
 
-new Vue({
-    el: '.game-window',
-    methods:{
+import Vue  from  '../../../node_modules/vue/dist/vue.js'; //'../../node_modules/vue/dist/vue.js';
+import VueSocketio from 'vue-socket.io';
 
-    }
+Vue.use(VueSocketio, 'http://localhost:3000/');
+
+document.addEventListener('DOMContentLoaded', function(){
+    const app = new Vue({
+        el: '.game-window',
+        sockets:{
+            connect: function(){
+                console.log('socket connected')
+            },
+            customEmit: function(val){
+                console.log('this method was fired by the socket server. eg: io.emit("customEmit", data)')
+            }
+        },
+        data: {
+            message: 'Hello, Singree!'
+        },
+        methods:{
+
+        }
+    });
+
 });
